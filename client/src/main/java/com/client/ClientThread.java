ad76625c3f05b8ae1604eb1ea4889a3f56ae2214
lai2-24 <lai2-24@itismeucci.com>
2023-11-21 14:12:27 +0100

    added sound notification of new received message 2

--- client/src/main/java/com/client/ClientThread.java
+++ client/src/main/java/com/client/ClientThread.java
@@ -6,9 +6,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import javax.sound.sampled.AudioInputStream; 
 import javax.sound.sampled.AudioSystem; 
-import javax.sound.sampled.Clip; 
-import javax.sound.sampled.LineUnavailableException; 
-import javax.sound.sampled.UnsupportedAudioFileException; 
+import javax.sound.sampled.Clip;
 
 public class ClientThread extends Thread{
     // to store current position 
